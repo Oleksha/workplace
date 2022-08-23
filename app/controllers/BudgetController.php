3abb843af89244abfa8d84a53608758904a8319e
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-08-23 15:43:16 +0400

    Update BudgetController.php

--- app/controllers/BudgetController.php
+++ app/controllers/BudgetController.php
@@ -164,8 +164,6 @@ class BudgetController extends AppController {
         unset($_SESSION['success']); // удаляем сессию с успешными сообщениями
         unset($_SESSION['error']);   // удаляем сессию с ошибочными сообщениями
         
-        //$this->updateTable();die;
-
         if (isset($_SESSION['file'])) {
             $reader = IOFactory::createReader('Xlsx');
             $spreadsheet = $reader->load(WWW . "/uploads/{$_SESSION['file']}");
@@ -329,50 +327,6 @@ class BudgetController extends AppController {
         $this->setMeta('Загрузка новых БО', '', '');
         // Передаем полученные данные в вид
         //$this->set(compact('receipt'));
-    }
-    
-    public function updateTable() {
-        $payments = \R::getAssocRow("SELECT * FROM payment");
-        foreach ($payments as $payment) {
-            $receipts_id = '';
-            $receipts = explode(';', $payment['receipt']);         
-            foreach ($receipts as $receipt) {
-                //debug($receipt);
-                $receipts_id .= $this->getReceipt($receipt) . ';';
-            }
-            $receipts_id = rtrim($receipts_id, ';');
-            $payment['receipts_id'] = $receipts_id;
-            $ers_id = '';
-            $ers = explode(';', $payment['num_er']); 
-            foreach ($ers as $er) {
-                //debug($receipt);
-                $ers_id .= $this->getER($er, $payment['id_partner']) . ';';
-            }
-            $ers_id = rtrim($ers_id, ';');
-            $payment['num_er_id'] = $ers_id;
-            debug($payment);
-        }        
-    }
-
-    public function getReceipt($numbers) {
-        //debug($numbers);
-        $numbers = explode('/', $numbers);
-        $number = $numbers[0];
-        $year = (int)$numbers[1];
-        //debug($number);debug($year);
-        $receipt = \R::getAssocRow("SELECT * FROM receipt WHERE YEAR(date) = {$year} AND number = '{$number}'");
-        //debug($receipt);
-        if (!empty($receipt)) { $receipt = $receipt[0]; return $receipt['id']; }
-        return false;
-    }
-
-    public function getER($number, $id_partner) {
-        //debug($number);debug($id_partner);
-        $er = \R::getAssocRow("SELECT * FROM er WHERE id_partner = {$id_partner} AND number = '{$number}'");
-        //debug($er);        
-        //debug($receipt);
-        if (!empty($er)) { $er = $er[0]; return $er['id']; }
-        return false;
     }
 
 }
