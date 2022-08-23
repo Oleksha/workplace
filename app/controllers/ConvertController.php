4041f92227d31fe39c962fa6828f556b8c889394
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-08-23 15:25:35 +0400

    Create ConvertController.php

---
+++ app/controllers/ConvertController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\controllers;
+
+class ConvertController extends AppController {
+
+    public function indexAction() {
+        
+        // формируем метатеги для страницы
+        $this->setMeta('Регистрация нового пользователя');
+
+    }
+
+}
