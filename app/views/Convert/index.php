81520b3d14b9cb18881a56ecd597a1a386cf9cf1
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-08-23 15:26:42 +0400

    Create index.php

---
+++ app/views/Convert/index.php
@@ -0,0 +1,15 @@
+<main role="main" class="flex-shrink-0">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <h1 class="mt-1">Конвертирование данных</h1>
+        </div>
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=PATH;?>">Главная</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Конвертирование данных</li>
+            </ol>
+        </nav>
+        
+    </div>
+</main>
